fb465a6a0e42a31cad4cdc5af8b80d75131337fb
Chikezie Blossom <chinanuchikezie515@gmail.com>
2024-04-13 20:29:52 +0100

    added arrow down image

--- public/index.ts
+++ public/index.ts
@@ -1,4 +1,6 @@
 import TIP_Logo from "./tip-logo.svg";
 import TipLogo from "./TIP-Logo.png";
+import ArrowDown from "./arrow-down.svg";
 
-export { TIP_Logo, TipLogo };
+
+export { TIP_Logo, TipLogo, ArrowDown };
