b6d2bfed74ff0d19a23109f1339186504b6dd14e
mxnuchim <manuchimoliver779@gmail.com>
2024-04-22 19:54:46 +0100

    feat: build fix 2

--- src/components/inputs/select-input/SelectInput.stories.tsx
+++ src/components/inputs/select-input/SelectInput.stories.tsx
@@ -2,18 +2,18 @@ import { Meta } from "@storybook/react";
 import SelectInput from "./SelectInput";
 import { SelectInputStory } from "./SelectInput.types";
 
-const SelectInputMeta = {
-    title: "Components/Inputs/SelectInput",
-    component: SelectInput,
-    tags: ["autodocs"],
-} satisfies Meta<typeof SelectInput>
+const SelectInputMeta: Meta = {
+  title: "Components/Inputs/SelectInput",
+  component: SelectInput,
+  tags: ["autodocs"],
+} satisfies Meta<typeof SelectInput>;
 
 export const DefaultStory: SelectInputStory = {
-    args: {
-        label: "Reason",
-        placeholder: "Let’s Know why you’re messaging...",
-        options: [{id: "business", name: "Business"}]
-    }
-}
+  args: {
+    label: "Reason",
+    placeholder: "Let’s Know why you’re messaging...",
+    options: [{ id: "business", name: "Business" }],
+  },
+};
 
 export default SelectInputMeta;
