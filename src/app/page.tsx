df009e536bd3815d9ab10aa0bd9246953d66566c
Chikezie Blossom <chinanuchikezie515@gmail.com>
2024-07-11 22:19:03 +0100

    build fix by adding use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,15 @@ import JobCategoryTemplateChunk from "@/chunks/home-template-chunk/job-category-
 import JobOpeningTemplateChunk from "@/chunks/home-template-chunk/job-opening-template-chunk/JobOpeningTemplateChunk";
 import TalentSectionTemplateChunk from "@/chunks/home-template-chunk/talent-section-template-chunk/TalentSectionTemplateChunk";
 import Navbar from "@/components/navbar/Navbar";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "The Intern Place",
-  description: "The Intern Place",
-};
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
+       <Head>
+        <title>The Intern Place</title>
+        <meta name="description" content={"The Intern Place"} />
+      </Head>
       <section className="bg-[#B5CAEC] pb-10">
         <Navbar />
         <HeroSectionTemplateChunk />
