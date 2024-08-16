05ca32ebad9e05a7d6d663eaca89165f35933cf3
Manuel <nuelay99@gmail.com>
2024-08-17 00:13:02 +0100

    another commit

--- src/chunks/job-description-chunk/job-description-tag-template-chunk/tag.tsx
+++ src/chunks/job-description-chunk/job-description-tag-template-chunk/tag.tsx
@@ -5,18 +5,17 @@ import React from "react";
 import JobDescriptionCard from "@/components/cards/job-description-card/JobDescriptionCard";
 
 function Tag() {
-    return (
-        <div className="w-full h-full sm:max-w-[1350px] md:pr-10 mt-10 mx-auto mb-6">
-            <JobDescriptionCard
-                img={" "}
-                title={"Social Media Assistant"}
-                company={"Urban"}
-                location={"Port-Harcourt, Nigeria"}
-                duration={"Full-Time"}
-            />
-        </div>
-
-    );
+  return (
+    <div className="w-full h-full sm:max-w-[1350px] md:pr-10 mt-10 mx-auto mb-6">
+      <JobDescriptionCard
+        img={" "}
+        title={"Social Media Assistant"}
+        company={"Urban"}
+        location={"Port-Harcourt, Nigeria"}
+        duration={"Full-Time"}
+      />
+    </div>
+  );
 }
 
 export default Tag;
