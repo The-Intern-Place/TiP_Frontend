766da0e9d1752e5a93c055d0d5e2cf154d5bee88
Oloruntobi <oloruntobialademehin@gmail.com>
2024-09-17 09:17:33 +0100

    done

---
+++ src/app/settings/layout.tsx
@@ -0,0 +1,23 @@
+import SettingsHeaderChunk from "@/chunks/settings-template-chunk/settings-header-template-chunk/SettingsHeaderChunk";
+import SidebarChunk from "@/chunks/settings-template-chunk/sidebar-template-chunk/SidebarChunk";
+import React, { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface ClassProps {
+  children: ReactNode;
+  className: string;
+}
+const layout = ({ children, className }: ClassProps) => {
+  const newClassName = twMerge("", className);
+  return (
+    <div>
+      <SidebarChunk />
+      <div className="">
+        <SettingsHeaderChunk />
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default layout;
---
+++ src/chunks/settings-template-chunk/settings-header-template-chunk/SettingsHeaderChunk.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SettingsHeaderChunk = () => {
+  return <div>SettingsHeaderChunk</div>;
+};
+
+export default SettingsHeaderChunk;
